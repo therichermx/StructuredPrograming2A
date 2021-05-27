fdab6af0852b1f1c0ea1198ae23e6eb6345b5b15
Rich <therichermx@gmail.com>
2021-05-27 09:25:51 -0500

    Unidad2

---
+++ Unit2/ArgMain.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+
+int main( int argc, char** argv)
+    printf( "argc: %i, element 1: %s, element 2: %s, char: %c \n", argc, argv[1], 'L');
+
+    return 0
---
+++ Unit2/ArgMain.py
@@ -0,0 +1,22 @@
+import sys
+
+##Variables##
+my_int = 0
+my_str = ""
+my_float = 0.0
+my_bool = True ##False
+my_list = [] ##list
+
+if __name__ == "__main__":
+
+    my_int = sys.argv[]
+    my_str = "Hola mundo"
+    my_float = 3.14
+    my_bool = False##False
+    my_list = [1,2,3,4] ##list
+
+    print(my_int,my_str,my_float,my_bool,my_list[1])
+
+    print("name program", sys.argv[1])
+
+    #print(sys.argv)
