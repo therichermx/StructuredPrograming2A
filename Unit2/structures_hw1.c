4b58f717d277fa7f03616c4a5328bba8b9cb2a4f
therichermx <therichermx@gmail.com>
2021-06-30 18:03:38 -0500

    Tarea Unit2

---
+++ Unit2/structures_hw1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include "utils.h"
+
+typedef struct ground
+	{
+		char Name[20];
+		char Type[40];
+		int range;
+		char tile[10];
+	}Ground;
+
+typedef struct naval
+    {
+    	char Name[20];
+		char Type[40];
+		int range;
+		char tile[10];
+    }Naval;
+
+
+int main(){
+    Ground Tank = {"Tank", "Ground", 5, "Plains"};
+    struct ground Ground = {"Tank", "Ground", 5, "Plains"};
+
+    Naval Cruiser = {"Cruiser", "Naval", 6, "Shoal"};
+    struct naval Naval = {"Cruiser", "Naval", 6, "Shoal"};
+
+
+}
