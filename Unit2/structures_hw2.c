c9d3e66f096f4a6ad182324c892f6b75a1c2e5c9
therichermx <therichermx@gmail.com>
2021-06-30 19:40:21 -0500

    structures_hw2

---
+++ Unit2/structures_hw2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "utils.h"
+
+
+int main(int argc, char** argv){
+
+        int otroInt  = 9;
+
+        UTILS myStructC = {1, ShowIntAddress, ShowIntValueAdress, ShowFloatAddress, ShowFloatValueAdress };
+        printf("myInt: %d. &myStructC: %p\n", myStructC.myInt, &myStructC );
+        UTILS* myStructP = &myStructC;
+
+        printf("myInt: %d\n", (*myStructP).myInt );
+        printf("myInt: %d\n", myStructP->myInt );
+
+        myStructC.ShowIntAdress(&otroInt );
+
+        return 0;
+}
--- Unit2/utils.h
+++ Unit2/utils.h
@@ -1,6 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define pi 3.1416
+
+
+//objeto//
+typedef struct utils {
+        int myInt;
+        void (*ShowIntAdress) (int* param); //a pointer
+        void (*ShowIntValueAdress)(  int* param);
+
+        void (*modifyVariablesAddress)( int* dir1   ,  int* dir2, int multiplo ){
+            ShowIntAddress(dir1);
+            ShowIntValueAdress(dir1);
+            ShowIntAdress(dir2);
+            ShowIntValueAdress(dir2);
+            *dir1 =  *dir2 * multiplo;
+            *dir2 =  *dir1 * multiplo;
+
+            return;
+        
+        }
+
+        void (*swapVariables)( int*ptr1, int*ptr2){
+            int temp = *ptr1;
+            *ptr1 = *ptr2;
+            *ptr2 = temp;
+            return;
+        }
+            
+        //void7+ swapGlobal(){
+        //int temp = myVar1;
+        //myVar1 = myVar2;
+        //myVar2 = temp return;
+        //}
+
+}UTILS;
+
+typedef struct  array
+{
+        int dirArray[2];
+        float aFloat;
+        float * aFloatPointer;
+
+}Array;
 
 int libInteger = 24;
 
@@ -17,6 +60,7 @@ int addTwoNumbers(int argumento1, int argumento2){
     return result;
 }
 
+//Paso de argumentos por valor//
 void modifyVariable (int argumento1, int argumento2){
 
     argumento1 = argumento1*2;
@@ -43,4 +87,41 @@ void modifyVariablesAddress( int* dir1   ,  int* dir2, int multiplo ){
 
         return;
         
+}
+
+void swapVariables( int*ptr1, int*ptr2){
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+    return;
+}
+//void swapGlobal(){
+//  int temp = myVar1;
+//myVar1 = myVar2;
+//myVar2 = temp return;
+//}
+
+void fillArray(int array[], size_t tam){
+    for (size_t i = 0; i < tam; i++)
+    {
+        array[i]= i*2;
+    }
+    return;
+};
+
+void printArray1D(int array[], size_t tam){
+    for (size_t i = 0; i < tam; i++)
+    {
+        printf("%i\n", array[i]);
+    }
+    return;
+}
+
+Array* returnArray(){
+    Array* unArrayType = (Array*)malloc(sizeof(Array));
+    printf("address unArrayType: %p, unArrayType = %p \n", &unArrayType, unArrayType    );
+
+    unArrayType->dirArray[0] = 17;
+    unArrayType->dirArray[1] = 15;
+    return unArrayType;
 }
