e0c1157d828cf849a74956e4309fa17749ffcdf6
therichermx <therichermx@gmail.com>
2021-07-25 12:43:36 -0500

    Proyecto Unidad 2

--- Unit2/Proyect/C1.py
+++ Unit2/Proyect/C1.py
@@ -5,7 +5,6 @@ tempF = {63, 73, -999, 91, 86, 82, 72, 81, 66, 77, 75, 104, -999, 84, 77, 66, 82
 
 print("Farenheit temperatures : " + str(tempF)) #original °f temp list
 
-
 celsius = [N for N in tempF if N > 0]
 
 print("Positive °F temperatures : " + str(celsius)) #reduced °f list
